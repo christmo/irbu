8d9a726d0d3615d56e2cc78b3afac7b8073d1ba8
christmo99 <christmo99@006dd04f-9d07-81d4-f290-3fe049d1e470>
2010-12-09 23:59:19 +0000

    Buscar Rutas GUI

--- IRBU/core/php/gui/comboRutas.php
+++ IRBU/core/php/gui/comboRutas.php
@@ -6,14 +6,20 @@ extract($_GET);
 
 $salida = "{failure:true}";
 
+//$consultaSql = "
+//            SELECT R.ID_RUTA, R.NOMBRE
+//            FROM RECORRIDO_RUTA R_R, RUTAS R
+//            WHERE ID_RECORRIDO='" . $id_rec . "' AND R_R.ID_RUTA = R.ID_RUTA AND R_R.ID_RUTA IN (
+//              SELECT RID.ID_RUTA
+//              FROM RUTAS RID
+//              WHERE RID.TIPO = '" . $op . "'
+//            )
+//            ";
+
 $consultaSql = "
-            SELECT R.ID_RUTA, R.NOMBRE
-            FROM RECORRIDO_RUTA R_R, RUTAS R
-            WHERE ID_RECORRIDO='" . $id_rec . "' AND R_R.ID_RUTA = R.ID_RUTA AND R_R.ID_RUTA IN (
-              SELECT RID.ID_RUTA
-              FROM RUTAS RID
-              WHERE RID.TIPO = '" . $op . "'
-            )
+            SELECT ID_RUTA,NOMBRE
+            FROM rutas
+            WHERE TIPO = '".$op."'
             ";
 
 consulta($consultaSql);
